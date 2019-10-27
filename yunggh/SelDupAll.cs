6e9594442a7be23a03d62f7050ec0eeb5e5fec20
archgame <matthewjosephconway@gmail.com>
2019-10-26 22:36:41 -0700

    Select all duplicates component added.

---
+++ yunggh/SelDupAll.cs
@@ -0,0 +1,163 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Grasshopper.Kernel;
+using Rhino.Geometry;
+
+// In order to load the result of this wizard, you will also need to
+// add the output bin/ folder of this project to the list of loaded
+// folder in Grasshopper.
+// You can use the _GrasshopperDeveloperSettings Rhino command for that.
+
+namespace GT
+{
+    public class SelDupAll : GH_Component
+    {
+        /// <summary>
+        /// Each implementation of GH_Component must provide a public 
+        /// constructor without any arguments.
+        /// Category represents the Tab in which the component will appear, 
+        /// Subcategory the panel. If you use non-existing tab or panel names, 
+        /// new tabs/panels will automatically be created.
+        /// </summary>
+        public SelDupAll()
+     : base("SelDupAll", "SelDupAll",
+       "Select all duplicates.",
+       "yung gh", "Selection")
+        {
+        }
+
+        /// <summary>
+        /// Registers all the input parameters for this component.
+        /// </summary>
+        protected override void RegisterInputParams(GH_Component.GH_InputParamManager pManager)
+        {
+            // Use the pManager object to register your input parameters.
+            // You can often supply default values when creating parameters.
+            // All parameters must have the correct access type. If you want 
+            // to import lists or trees of values, modify the ParamAccess flag.
+            pManager.AddBooleanParameter("Prompt", "P", "Boolean to prompt user for input.", GH_ParamAccess.item);
+
+            // If you want to change properties of certain parameters, 
+            // you can use the pManager instance to access them by index:
+            //pManager[0].Optional = true;
+        }
+
+        /// <summary>
+        /// Registers all the output parameters for this component.
+        /// </summary>
+        protected override void RegisterOutputParams(GH_Component.GH_OutputParamManager pManager)
+        {
+            // Use the pManager object to register your output parameters.
+            // Output parameters do not have default values, but they too must have the correct access type.
+            pManager.AddGenericParameter("Geometry", "G", "All Duplicates", GH_ParamAccess.list);
+            //pManager.AddGeometryParameter("Geometry", "G", "All Duplicates", GH_ParamAccess.item);
+            pManager.AddTextParameter("GUID", "ID", "GUIDs", GH_ParamAccess.list);
+            // Sometimes you want to hide a specific parameter from the Rhino preview.
+            // You can use the HideParameter() method as a quick way:
+            //pManager.HideParameter(0);
+        }
+
+        /// <summary>
+        /// This is the method that actually does the work.
+        /// </summary>
+        /// <param name="DA">The DA object can be used to retrieve data from input parameters and 
+        /// to store data in output parameters.</param>
+        protected override void SolveInstance(IGH_DataAccess DA)
+        {
+            // First, we need to retrieve all data from the input parameters.
+            // We'll start by declaring variables and assigning them starting values.
+            bool run = false;
+
+            // Then we need to access the input parameters individually. 
+            // When data cannot be extracted from a parameter, we should abort this method.
+            if (!DA.GetData(0, ref run)) return;
+
+            // We're set to create the spiral now. To keep the size of the SolveInstance() method small, 
+            // The actual functionality will be in a different method:
+            List<GeometryBase> geometry = new List<GeometryBase>();
+            List<string> guids = new List<string>();
+            IEnumerable<Rhino.DocObjects.RhinoObject> selected = GetSelDupAll(run);
+            if (selected != null)
+            {
+
+                foreach (Rhino.DocObjects.RhinoObject ro in selected)
+                {
+                    geometry.Add(ro.Geometry);
+                    guids.Add(ro.Id.ToString());
+                }
+
+            }
+            DA.SetDataList(0, geometry);
+            DA.SetDataList(1, guids);
+        }
+
+        private IEnumerable<Rhino.DocObjects.RhinoObject> GetSelDupAll(bool select)
+        {
+            if (!select && !pending) //return when button isn't pressed
+            {
+                return output;
+            }
+            if (!pending) //return & set pending to true
+            {
+                pending = true;
+                return output;
+            }
+
+            if (pending) //pending
+            {
+                Rhino.RhinoApp.RunScript("_SelDupAll", false);
+                IEnumerable<Rhino.DocObjects.RhinoObject> objects = Rhino.RhinoDoc.ActiveDoc.Objects.GetSelectedObjects(false, false);
+                //List<GeometryBase> geo = new List<GeometryBase>();
+                //foreach (Rhino.DocObjects.RhinoObject ro in objects)
+                //{
+                //Print(ro.Id.ToString());
+                //geo.Add(ro.Geometry);
+                //}
+
+                output = objects;
+            }
+
+            pending = false;
+            return output;
+
+        }
+        public IEnumerable<Rhino.DocObjects.RhinoObject> output;
+        private bool pending = false;
+
+        /// <summary>
+        /// The Exposure property controls where in the panel a component icon 
+        /// will appear. There are seven possible locations (primary to septenary), 
+        /// each of which can be combined with the GH_Exposure.obscure flag, which 
+        /// ensures the component will only be visible on panel dropdowns.
+        /// </summary>
+        public override GH_Exposure Exposure
+        {
+            get { return GH_Exposure.primary; }
+        }
+
+        /// <summary>
+        /// Provides an Icon for every component that will be visible in the User Interface.
+        /// Icons need to be 24x24 pixels.
+        /// </summary>
+        protected override System.Drawing.Bitmap Icon
+        {
+            get
+            {
+                // You can add image files to your project resources and access them like this:
+                //return Resources.IconForThisComponent;
+                return null;
+            }
+        }
+
+        /// <summary>
+        /// Each component must have a unique Guid to identify it. 
+        /// It is vital this Guid doesn't change otherwise old ghx files 
+        /// that use the old ID will partially fail during loading.
+        /// </summary>
+        public override Guid ComponentGuid
+        {
+            get { return new Guid("43d68cf2-f346-4467-a302-96162f0bd559"); }
+        }
+    }
+}

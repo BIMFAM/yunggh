a48b6439dac18866a6ee25c5e3c291975812aaba
archgame <matthewjosephconway@gmail.com>
2019-12-22 08:18:08 -0800

    index output added.

--- yunggh/LargestNumber.cs
+++ yunggh/LargestNumber.cs
@@ -12,112 +12,114 @@ using Rhino.Geometry;
 // folder in Grasshopper.
 // You can use the _GrasshopperDeveloperSettings Rhino command for that.
 
-namespace GT
+namespace yunggh
 {
     public class LargestNumber : GH_Component
     {
-        /// <summary>
-        /// Each implementation of GH_Component must provide a public 
-        /// constructor without any arguments.
-        /// Category represents the Tab in which the component will appear, 
-        /// Subcategory the panel. If you use non-existing tab or panel names, 
-        /// new tabs/panels will automatically be created.
-        /// </summary>
-        public LargestNumber()
-     : base("LargestNumber", "LargestNumber",
-       "Return largest number from collection",
-       "yung gh", "Data")
+        /// <summary>
+        /// Each implementation of GH_Component must provide a public
+        /// constructor without any arguments.
+        /// Category represents the Tab in which the component will appear,
+        /// Subcategory the panel. If you use non-existing tab or panel names,
+        /// new tabs/panels will automatically be created.
+        /// </summary>
+        public LargestNumber()
+          : base("LargestNumber", "LargestNumber",
+              "Return largest number from collection",
+              "yung gh", "Data")
         {
         }
 
-        /// <summary>
-        /// Registers all the input parameters for this component.
-        /// </summary>
-        protected override void RegisterInputParams(GH_Component.GH_InputParamManager pManager)
+        /// <summary>
+        /// Registers all the input parameters for this component.
+        /// </summary>
+        protected override void RegisterInputParams(GH_Component.GH_InputParamManager pManager)
         {
-            // Use the pManager object to register your input parameters.
-            // You can often supply default values when creating parameters.
-            // All parameters must have the correct access type. If you want 
-            // to import lists or trees of values, modify the ParamAccess flag.
-            pManager.AddNumberParameter("Numbers", "N", "List of numbers", GH_ParamAccess.list);
-            // If you want to change properties of certain parameters, 
-            // you can use the pManager instance to access them by index:
-            //pManager[0].Optional = true;
-        }
+            // Use the pManager object to register your input parameters.
+            // You can often supply default values when creating parameters.
+            // All parameters must have the correct access type. If you want
+            // to import lists or trees of values, modify the ParamAccess flag.
+            pManager.AddNumberParameter("Numbers", "N", "List of numbers", GH_ParamAccess.list);
+            // If you want to change properties of certain parameters,
+            // you can use the pManager instance to access them by index:
+            //pManager[0].Optional = true;
+        }
 
-        /// <summary>
-        /// Registers all the output parameters for this component.
-        /// </summary>
-        protected override void RegisterOutputParams(GH_Component.GH_OutputParamManager pManager)
+        /// <summary>
+        /// Registers all the output parameters for this component.
+        /// </summary>
+        protected override void RegisterOutputParams(GH_Component.GH_OutputParamManager pManager)
         {
-            // Use the pManager object to register your output parameters.
-            // Output parameters do not have default values, but they too must have the correct access type.
-            pManager.AddNumberParameter("Number", "N", "Largest Number", GH_ParamAccess.item);
-            // Sometimes you want to hide a specific parameter from the Rhino preview.
-            // You can use the HideParameter() method as a quick way:
-            //pManager.HideParameter(0);
-        }
+            // Use the pManager object to register your output parameters.
+            // Output parameters do not have default values, but they too must have the correct access type.
+            pManager.AddNumberParameter("Number", "N", "Largest Number", GH_ParamAccess.item);
+            pManager.AddNumberParameter("Index", "I", "Largest Number Index", GH_ParamAccess.item);
+            // Sometimes you want to hide a specific parameter from the Rhino preview.
+            // You can use the HideParameter() method as a quick way:
+            //pManager.HideParameter(0);
+        }
 
-        /// <summary>
-        /// This is the method that actually does the work.
-        /// </summary>
-        /// <param name="DA">The DA object can be used to retrieve data from input parameters and 
-        /// to store data in output parameters.</param>
-        protected override void SolveInstance(IGH_DataAccess DA)
+        /// <summary>
+        /// This is the method that actually does the work.
+        /// </summary>
+        /// <param name="DA">The DA object can be used to retrieve data from input parameters and
+        /// to store data in output parameters.</param>
+        protected override void SolveInstance(IGH_DataAccess DA)
         {
-            // First, we need to retrieve all data from the input parameters.
-            // We'll start by declaring variables and assigning them starting values.
-            List<double> data = new List<double>();
+            // First, we need to retrieve all data from the input parameters.
+            // We'll start by declaring variables and assigning them starting values.
+            List<double> data = new List<double>();
 
-            // Then we need to access the input parameters individually. 
-            // When data cannot be extracted from a parameter, we should abort this method.
-            if (!DA.GetDataList(0, data)) return;
-            // We should now validate the data and warn the user if invalid data is supplied.
-            if (data.Count == 0)
+            // Then we need to access the input parameters individually.
+            // When data cannot be extracted from a parameter, we should abort this method.
+            if (!DA.GetDataList(0, data)) return;
+            // We should now validate the data and warn the user if invalid data is supplied.
+            if (data.Count == 0)
             {
                 AddRuntimeMessage(GH_RuntimeMessageLevel.Error, "No data input");
                 return;
             }
 
-            // We're set to create the spiral now. To keep the size of the SolveInstance() method small, 
+            // We're set to create the spiral now. To keep the size of the SolveInstance() method small,
             // The actual functionality will be in a different method:
             double largest = data.Max();
-
-            // Finally assign the spiral to the output parameter.
-            DA.SetData(0, largest);
+            int index = data.IndexOf(largest);
+            //assign output parameters.
+            DA.SetData(0, largest);
+            DA.SetData(1, index);
         }
 
-        /// <summary>
-        /// The Exposure property controls where in the panel a component icon 
-        /// will appear. There are seven possible locations (primary to septenary), 
-        /// each of which can be combined with the GH_Exposure.obscure flag, which 
-        /// ensures the component will only be visible on panel dropdowns.
-        /// </summary>
-        public override GH_Exposure Exposure
+        /// <summary>
+        /// The Exposure property controls where in the panel a component icon
+        /// will appear. There are seven possible locations (primary to septenary),
+        /// each of which can be combined with the GH_Exposure.obscure flag, which
+        /// ensures the component will only be visible on panel dropdowns.
+        /// </summary>
+        public override GH_Exposure Exposure
         {
             get { return GH_Exposure.primary; }
         }
 
-        /// <summary>
-        /// Provides an Icon for every component that will be visible in the User Interface.
-        /// Icons need to be 24x24 pixels.
-        /// </summary>
-        protected override System.Drawing.Bitmap Icon
+        /// <summary>
+        /// Provides an Icon for every component that will be visible in the User Interface.
+        /// Icons need to be 24x24 pixels.
+        /// </summary>
+        protected override System.Drawing.Bitmap Icon
         {
             get
             {
-                // You can add image files to your project resources and access them like this:
-                //return Resources.IconForThisComponent;
-                return null;
+                // You can add image files to your project resources and access them like this:
+                //return Resources.IconForThisComponent;
+                return null;
             }
         }
 
-        /// <summary>
-        /// Each component must have a unique Guid to identify it. 
-        /// It is vital this Guid doesn't change otherwise old ghx files 
-        /// that use the old ID will partially fail during loading.
-        /// </summary>
-        public override Guid ComponentGuid
+        /// <summary>
+        /// Each component must have a unique Guid to identify it.
+        /// It is vital this Guid doesn't change otherwise old ghx files
+        /// that use the old ID will partially fail during loading.
+        /// </summary>
+        public override Guid ComponentGuid
         {
             get { return new Guid("43d68cf2-f346-4467-a302-96162f0bd553"); }
         }

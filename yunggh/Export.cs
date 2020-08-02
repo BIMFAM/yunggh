761f1a46e338e256682dbe79695a50b74ce80c0e
archgame <matthewjosephconway@gmail.com>
2020-08-02 16:03:19 -0700

    Updated Export component.

--- yunggh/Export.cs
+++ yunggh/Export.cs
@@ -1,8 +1,16 @@
 ﻿using System;
 using System.Collections.Generic;
 
-using Grasshopper.Kernel;
+using Rhino;
 using Rhino.Geometry;
+
+using Grasshopper;
+using Grasshopper.Kernel;
+using Grasshopper.Kernel.Data;
+using Grasshopper.Kernel.Types;
+
+using System.IO;
+using System.Linq;
 
 // In order to load the result of this wizard, you will also need to
 // add the output bin/ folder of this project to the list of loaded
@@ -14,10 +22,10 @@ namespace yunggh
     public class Export : GH_Component
     {
         /// <summary>
-        /// Each implementation of GH_Component must provide a public 
+        /// Each implementation of GH_Component must provide a public
         /// constructor without any arguments.
-        /// Category represents the Tab in which the component will appear, 
-        /// Subcategory the panel. If you use non-existing tab or panel names, 
+        /// Category represents the Tab in which the component will appear,
+        /// Subcategory the panel. If you use non-existing tab or panel names,
         /// new tabs/panels will automatically be created.
         /// </summary>
         public Export()
@@ -32,18 +40,9 @@ namespace yunggh
         /// </summary>
         protected override void RegisterInputParams(GH_Component.GH_InputParamManager pManager)
         {
-            // Use the pManager object to register your input parameters.
-            // You can often supply default values when creating parameters.
-            // All parameters must have the correct access type. If you want 
-            // to import lists or trees of values, modify the ParamAccess flag.
-            pManager.AddPlaneParameter("Plane", "P", "Base plane for spiral", GH_ParamAccess.item, Plane.WorldXY);
-            pManager.AddNumberParameter("Inner Radius", "R0", "Inner radius for spiral", GH_ParamAccess.item, 1.0);
-            pManager.AddNumberParameter("Outer Radius", "R1", "Outer radius for spiral", GH_ParamAccess.item, 10.0);
-            pManager.AddIntegerParameter("Turns", "T", "Number of turns between radii", GH_ParamAccess.item, 10);
-
-            // If you want to change properties of certain parameters, 
-            // you can use the pManager instance to access them by index:
-            //pManager[0].Optional = true;
+            pManager.AddBooleanParameter("Export", "E", "Run Export", GH_ParamAccess.item);
+            pManager.AddGenericParameter("Objects", "O", "Objects to Export", GH_ParamAccess.tree);
+            pManager.AddTextParameter("Filepaths", "F", "Export Filepath", GH_ParamAccess.tree);
         }
 
         /// <summary>
@@ -51,90 +50,116 @@ namespace yunggh
         /// </summary>
         protected override void RegisterOutputParams(GH_Component.GH_OutputParamManager pManager)
         {
-            // Use the pManager object to register your output parameters.
-            // Output parameters do not have default values, but they too must have the correct access type.
-            pManager.AddCurveParameter("Spiral", "S", "Spiral curve", GH_ParamAccess.item);
-
-            // Sometimes you want to hide a specific parameter from the Rhino preview.
-            // You can use the HideParameter() method as a quick way:
-            //pManager.HideParameter(0);
+            pManager.AddTextParameter("Exported Files", "E", "Exported Filepaths", GH_ParamAccess.tree);
         }
 
         /// <summary>
         /// This is the method that actually does the work.
         /// </summary>
-        /// <param name="DA">The DA object can be used to retrieve data from input parameters and 
+        /// <param name="DA">The DA object can be used to retrieve data from input parameters and
         /// to store data in output parameters.</param>
         protected override void SolveInstance(IGH_DataAccess DA)
         {
             // First, we need to retrieve all data from the input parameters.
             // We'll start by declaring variables and assigning them starting values.
-            Plane plane = Plane.WorldXY;
-            double radius0 = 0.0;
-            double radius1 = 0.0;
-            int turns = 0;
+            bool run = false;
+            GH_Structure<IGH_Goo> objects;
+            GH_Structure<GH_String> inputFilePaths;
 
-            // Then we need to access the input parameters individually. 
+            // Then we need to access the input parameters individually.
             // When data cannot be extracted from a parameter, we should abort this method.
-            if (!DA.GetData(0, ref plane)) return;
-            if (!DA.GetData(1, ref radius0)) return;
-            if (!DA.GetData(2, ref radius1)) return;
-            if (!DA.GetData(3, ref turns)) return;
+            if (!DA.GetData(0, ref run)) return;
+            if (!DA.GetDataTree<IGH_Goo>(1, out objects)) return;
+            if (!DA.GetDataTree<GH_String>(2, out inputFilePaths)) return;
 
-            // We should now validate the data and warn the user if invalid data is supplied.
-            if (radius0 < 0.0)
+            // main
+
+            //return when button isn't pressed
+            if (!run && !pending) return;
+
+            //return & set pending to true
+            if (!pending)
+            { pending = true; return; }
+
+            // reset pending to false
+            pending = false;
+
+            //establish a clean data tree
+            filePaths = new GH_Structure<GH_String>();
+
+            //loop through all the input filepath paths
+            foreach (GH_Path path in inputFilePaths.Paths)
             {
-                AddRuntimeMessage(GH_RuntimeMessageLevel.Error, "Inner radius must be bigger than or equal to zero");
-                return;
-            }
-            if (radius1 <= radius0)
-            {
-                AddRuntimeMessage(GH_RuntimeMessageLevel.Error, "Outer radius must be bigger than the inner radius");
-                return;
-            }
-            if (turns <= 0)
-            {
-                AddRuntimeMessage(GH_RuntimeMessageLevel.Error, "Spiral turn count must be bigger than or equal to one");
-                return;
+                Rhino.RhinoDoc.ActiveDoc.Objects.UnselectAll();
+                List<System.Guid> guids = new List<System.Guid>();
+
+                //select all objects
+                IGH_Goo[] gh_guids = objects[path].ToArray();
+                for (int j = 0; j < gh_guids.Length; j++)
+                {
+                    GH_Guid gh_guid = (GH_Guid)gh_guids[j];
+                    if (gh_guid == null) continue;
+
+                    System.Guid guid = gh_guid.Value;
+                    guids.Add(guid);
+                }
+
+                Select(Rhino.RhinoDoc.ActiveDoc, guids);
+
+                //export objects
+                GH_String[] filepaths = inputFilePaths[path].ToArray();
+                for (int j = 0; j < filepaths.Length; j++)
+                {
+                    string filepath = filepaths[j].Value;
+
+                    //if the file type is not supported, we skip it
+                    if (!SupportedExportFileTypes.Contains(Path.GetExtension(filepath))) continue;
+
+                    //export
+                    ExportModel(filepath);
+
+                    GH_Path newPath = new GH_Path(path);
+                    filePaths.AppendRange(new List<GH_String>() { new GH_String(filepath) }, newPath);
+                }
+
+                Rhino.RhinoDoc.ActiveDoc.Objects.UnselectAll();
             }
 
-            // We're set to create the spiral now. To keep the size of the SolveInstance() method small, 
-            // The actual functionality will be in a different method:
-            Curve spiral = CreateSpiral(plane, radius0, radius1, turns);
-
-            // Finally assign the spiral to the output parameter.
-            DA.SetData(0, spiral);
+            // Assign the export filepaths to the output parameter.
+            DA.SetDataTree(0, filePaths);
         }
 
-        private Curve CreateSpiral(Plane plane, double r0, double r1, Int32 turns)
+        private bool pending = false;
+
+        private GH_Structure<GH_String> filePaths;
+
+        public void Select(RhinoDoc doc, List<System.Guid> guids)
         {
-            Line l0 = new Line(plane.Origin + r0 * plane.XAxis, plane.Origin + r1 * plane.XAxis);
-            Line l1 = new Line(plane.Origin - r0 * plane.XAxis, plane.Origin - r1 * plane.XAxis);
-
-            Point3d[] p0;
-            Point3d[] p1;
-
-            l0.ToNurbsCurve().DivideByCount(turns, true, out p0);
-            l1.ToNurbsCurve().DivideByCount(turns, true, out p1);
-
-            PolyCurve spiral = new PolyCurve();
-
-            for (int i = 0; i < p0.Length - 1; i++)
-            {
-                Arc arc0 = new Arc(p0[i], plane.YAxis, p1[i + 1]);
-                Arc arc1 = new Arc(p1[i + 1], -plane.YAxis, p0[i + 1]);
-
-                spiral.Append(arc0);
-                spiral.Append(arc1);
-            }
-
-            return spiral;
+            foreach (System.Guid guid in guids)
+                doc.Objects.Select(guid, true, true, true);
         }
+
+        public void ExportModel(string filepath)
+        {
+            string scriptExport = string.Format("_-Export \"{0}\" _Enter", filepath);
+            RhinoApp.RunScript(scriptExport, false);
+        }
+
+        public List<string> SupportedExportFileTypes = new List<string>(){
+      ".3dm",".3dmbak",".rws",".3mf",".3ds",".amf",".ai",
+      ".dwg",".dxf",".x",
+      ".gf",".gft",".gts",".igs",".iges",".lwo",".dgn",".fbx",
+      ".obj",".pdf",".ply",".csv",
+      ".txt",".raw",".m",".svg",".skp",".slc",
+      ".stp",".step",".stl",".vda",".wrl",
+      ".vrml",".vi",".gdf",".zpr",
+      ".dae",".cd",".emf",".pm",".kmz",".udo",".x_t",".rib",".wmf",".x3dv",".xaml",".xgl"
+      };
 
         /// <summary>
-        /// The Exposure property controls where in the panel a component icon 
-        /// will appear. There are seven possible locations (primary to septenary), 
-        /// each of which can be combined with the GH_Exposure.obscure flag, which 
+        /// The Exposure property controls where in the panel a component icon
+        /// will appear. There are seven possible locations (primary to septenary),
+        /// each of which can be combined with the GH_Exposure.obscure flag, which
         /// ensures the component will only be visible on panel dropdowns.
         /// </summary>
         public override GH_Exposure Exposure
@@ -150,15 +175,13 @@ namespace yunggh
         {
             get
             {
-                // You can add image files to your project resources and access them like this:
-                //return Resources.IconForThisComponent;
-                return null;
+                return Resource.Export;
             }
         }
 
         /// <summary>
-        /// Each component must have a unique Guid to identify it. 
-        /// It is vital this Guid doesn't change otherwise old ghx files 
+        /// Each component must have a unique Guid to identify it.
+        /// It is vital this Guid doesn't change otherwise old ghx files
         /// that use the old ID will partially fail during loading.
         /// </summary>
         public override Guid ComponentGuid

19e1f5d66e8d6093ca849cc9fca9e2c66376b083
archgame <matthewjosephconway@gmail.com>
2020-08-15 18:37:22 -0700

    Added ShrinkSurface component.

---
+++ yunggh/Components/Geometry/ShrinkSurface.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Grasshopper.Kernel;
+using Rhino.Geometry;
+
+// In order to load the result of this wizard, you will also need to
+// add the output bin/ folder of this project to the list of loaded
+// folder in Grasshopper.
+// You can use the _GrasshopperDeveloperSettings Rhino command for that.
+
+namespace yunggh
+{
+    public class ShrinkSurface : GH_Component
+    {
+        /// <summary>
+        /// Each implementation of GH_Component must provide a public
+        /// constructor without any arguments.
+        /// Category represents the Tab in which the component will appear,
+        /// Subcategory the panel. If you use non-existing tab or panel names,
+        /// new tabs/panels will automatically be created.
+        /// </summary>
+        public ShrinkSurface()
+          : base("Shrink Surface", "SNK",
+              "Shrink a Trimmed Surface to a minimum surface.",
+              "yung gh", "Geometry")
+        {
+            CustomAttributes custom = new CustomAttributes(this);
+            this.m_attributes = custom;
+        }
+
+        /// <summary>
+        /// Registers all the input parameters for this component.
+        /// </summary>
+        protected override void RegisterInputParams(GH_Component.GH_InputParamManager pManager)
+        {
+            pManager.AddSurfaceParameter("Surface", "S", "Surface to shrink.", GH_ParamAccess.item);
+            pManager.AddIntegerParameter("Disable Side", "I", "0 = Shrink All Sides, 1 = Disable North, 2 = Disable East, 3 = Disable South, 4 = Disable West", GH_ParamAccess.item, 0);
+        }
+
+        /// <summary>
+        /// Registers all the output parameters for this component.
+        /// </summary>
+        protected override void RegisterOutputParams(GH_Component.GH_OutputParamManager pManager)
+        {
+            pManager.AddBrepParameter("Surface", "S", "Shrunk Surface", GH_ParamAccess.item);
+        }
+
+        /// <summary>
+        /// This is the method that actually does the work.
+        /// </summary>
+        /// <param name="DA">The DA object can be used to retrieve data from input parameters and
+        /// to store data in output parameters.</param>
+        protected override void SolveInstance(IGH_DataAccess DA)
+        {
+            // Retrieve all data from the input parameters (start by declaring variables and assigning them starting values).
+            Brep surface = null;
+            int disableSides = 0;
+
+            // guard statement for when data cannot be extracted from a parameter
+            if (!DA.GetData(0, ref surface)) return;
+            if (!DA.GetData(1, ref disableSides)) return;
+
+            // warnings
+            if (surface == null)
+            {
+                AddRuntimeMessage(GH_RuntimeMessageLevel.Error, "Surface is null");
+                return;
+            }
+
+            // main
+            YungGH yunggh = new YungGH();
+            Brep surfaceShrunk = yunggh.ShrinkSurface(surface, disableSides);
+
+            // Finally assign the surface to the output parameter.
+            DA.SetData(0, surfaceShrunk);
+        }
+
+        /// <summary>
+        /// The Exposure property controls where in the panel a component icon
+        /// will appear. There are seven possible locations (primary to septenary),
+        /// each of which can be combined with the GH_Exposure.obscure flag, which
+        /// ensures the component will only be visible on panel dropdowns.
+        /// </summary>
+        public override GH_Exposure Exposure
+        {
+            get { return GH_Exposure.primary; }
+        }
+
+        /// <summary>
+        /// Provides an Icon for every component that will be visible in the User Interface.
+        /// Icons need to be 24x24 pixels.
+        /// </summary>
+        protected override System.Drawing.Bitmap Icon
+        {
+            get
+            {
+                return Resource.ShrinkSurface;
+            }
+        }
+
+        /// <summary>
+        /// Each component must have a unique Guid to identify it.
+        /// It is vital this Guid doesn't change otherwise old ghx files
+        /// that use the old ID will partially fail during loading.
+        /// </summary>
+        public override Guid ComponentGuid
+        {
+            get { return new Guid("c255817d-c846-4469-b251-8611c054f73e"); }
+        }
+    }
+}

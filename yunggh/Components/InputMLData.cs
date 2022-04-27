7fe86509dd00d0a37fa7494916d1a2d59715817c
Jenson93 <chendejian1993@gmail.com>
2022-04-26 21:45:41 -0700

    Kmeans workflow

---
+++ yunggh/Components/InputMLData.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.ML.Data;
+
+namespace yunggh.Components
+{
+    public class InputData
+    {
+        // for loop
+        [ColumnName("feature1")]
+        public float input1;
+
+        [ColumnName("feature2")]
+        public float input2;
+        // loop all columns
+
+        // public float var 1
+    }
+
+    public class ClusterPrediction
+    {
+        [ColumnName("PredictedLabel")]
+        public uint PredictedClusterId;
+
+        [ColumnName("score")]
+        public float[] Distances;
+    }
+}

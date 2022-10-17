aa781d06cc70b3d6ece376c60d8b7dbc2f388a07
Matt Conway <matthewjosephconway@gmail.com>
2022-10-17 10:24:16 -0700

    Working ExportByMaterial

--- yunggh/Components/Format/ExportByMaterial.cs
+++ yunggh/Components/Format/ExportByMaterial.cs
@@ -12,6 +12,7 @@ using Grasshopper.Kernel.Types;
 
 using System.IO;
 using System.Linq;
+using System.Drawing;
 
 // In order to load the result of this wizard, you will also need to
 // add the output bin/ folder of this project to the list of loaded
@@ -62,21 +63,129 @@ namespace yunggh
         {
             //Get input data from Grasshopper
             bool run = false;
-            string inputFilePath = string.Empty;
+            string filepath = string.Empty;
             bool keep = false;
             if (!DA.GetData(0, ref run)) return;
-            if (!DA.GetData(1, ref inputFilePath)) return;
+            if (!DA.GetData(1, ref filepath)) return;
             if (!DA.GetData(2, ref keep)) return;
+
+            //Grasshopper Button Issue Fix
+            if (!run && !pending) return; //return when button isn't pressed
+            if (!pending) { pending = true; return; }//return & set pending to true
+            pending = false; // reset pending to false
 
             //get all objects
             var guids = YungGH.GetGuids(); Debug.WriteLine("Guids.Count: " + guids.Count);
 
-            //combine all objects
+            //organize objects by layer
+            var doc = Rhino.RhinoDoc.ActiveDoc;
+            Dictionary<int, List<GeometryBase>> objects = new Dictionary<int, List<GeometryBase>>();
+            foreach (var guid in guids)
+            {
+                var rhobj = doc.Objects.FindId(guid);
+                if (rhobj == null) { continue; }
+                int materialIndex = rhobj.Attributes.MaterialIndex;
+                if(materialIndex == -1) //get layer material if there isn't one on the object
+                {
+                    int layerIndex = rhobj.Attributes.LayerIndex;
+                    var layer = RhinoDoc.ActiveDoc.Layers.FindIndex(layerIndex);
+                    materialIndex = layer.RenderMaterialIndex;
+                }
+                if (!objects.ContainsKey(materialIndex))
+                {
+                    objects.Add(materialIndex, new List<GeometryBase>());
+                }
+                objects[materialIndex].Add(rhobj.Geometry);
+            }
 
-            //bake objects as mesh
+            //combine objects into Meshes
+            Dictionary<string, GeometryBase> joinedMeshes = new Dictionary<string, GeometryBase>();
+            foreach (KeyValuePair<int, List<GeometryBase>> kvp in objects)
+            {
+                //get layer name
+                var materialIndex = kvp.Key;
+                var mat = RhinoDoc.ActiveDoc.Materials.FindIndex(materialIndex);
+
+                string materialName = "NoMaterialSelected";
+                if(materialIndex != -1)
+                {
+                    materialName = RhinoDoc.ActiveDoc.Materials.FindIndex(materialIndex).Name;
+                }
+                var rhobjs = kvp.Value;
+                Mesh mesh = new Mesh();
+                foreach (var rhobj in rhobjs)
+                {
+                    if (rhobj is Mesh)
+                    {
+                        Mesh m = rhobj as Mesh;
+                        mesh.Append(m);
+                        continue;
+                    }
+                    if (rhobj is Brep)
+                    {
+                        Brep brep = rhobj as Brep;
+                        var mesh_params = MeshingParameters.FastRenderMesh;
+                        var meshes = Mesh.CreateFromBrep(brep, mesh_params);
+                        foreach (var m in meshes) { mesh.Append(m); }
+                    }
+                    if (rhobj is Surface)
+                    {
+                        Surface srf = rhobj as Surface;
+                        var mesh_params = MeshingParameters.Default;
+                        var m = Mesh.CreateFromSurface(srf, mesh_params);
+                        mesh.Append(m);
+                    }
+                }
+                if(joinedMeshes.ContainsKey(materialName))
+                {
+                    var geo = joinedMeshes[materialName];
+                    Mesh m = geo as Mesh;
+                    mesh.Append(m);
+                    joinedMeshes[materialName] = mesh;
+                    continue;
+                }
+
+                joinedMeshes.Add(materialName, mesh);
+            }
+
+            //make basic layer to hold all the meshes
+            string fullLayerPath = "Materials";
+            Color color = Color.Black;
+            string material = "";
+            bool locked = false;
+            bool onOff = true;
+            string plotLineType = "";
+            Color plotLineColor = Color.Black; 
+            double plotLineWidth = 1.0;
+            bool delete = false;
+            YungGH.CreateModify(Rhino.RhinoDoc.ActiveDoc, fullLayerPath, color, locked,
+                plotLineType, material, onOff, plotLineColor, plotLineWidth, delete);
+
+            //bake objects as mesh by material
+            List<GeometryBase> geoBase = joinedMeshes.Values.ToList();
+            List<string> layers = new List<string>();
+            List<string> materials = joinedMeshes.Keys.ToList();
+            for (int i = 0; i < materials.Count; i++)
+            {
+                layers.Add(fullLayerPath);
+            }
+
+            guids = YungGH.BakeGeometry(geoBase, layers, materials); //TODO: Make sure objects have material of layer name assigned
 
             //export objects
+            YungGH.Select(Rhino.RhinoDoc.ActiveDoc, guids);
+            if (Path.GetExtension(filepath) != ".fbx")
+            {
+                filepath = Path.ChangeExtension(filepath, ".fbx");
+            }
+            YungGH.ExportModel(filepath);
+
+            //delete baked elements if keep is false
+            if (keep) { return; }
+            doc.Objects.Delete(guids, true);
         }
+
+        private bool pending = false;
 
         /// <summary>
         /// The Exposure property controls where in the panel a component icon

1bb57ac992f3dfb5028dafcdb6add20e606e720e
xuanhe95 <xuanhe95@gmail.com>
2023-01-27 12:05:01 -0800

    Erosion Implementation

--- yunggh/Components/Shape/Erosion.cs
+++ yunggh/Components/Shape/Erosion.cs
@@ -17,14 +17,53 @@ namespace yunggh
 
         protected override void RegisterInputParams(GH_Component.GH_InputParamManager pManager)
         {
+            pManager.AddCurveParameter("Curve", "C", "Curve for erosion", GH_ParamAccess.item);
+            pManager.AddNumberParameter("Double", "D", "Double for distance of each curve", GH_ParamAccess.item);
         }
 
         protected override void RegisterOutputParams(GH_Component.GH_OutputParamManager pManager)
         {
+            pManager.AddCurveParameter("Curve", "C", "Curves for erossion", GH_ParamAccess.list);
         }
 
         protected override void SolveInstance(IGH_DataAccess DA)
         {
+            Curve crv = null;
+            Double step = 0;
+            if (!DA.GetData(0, ref crv) || !DA.GetData(1, ref step)) return;
+            List<Curve> output = new List<Curve>();//output list
+            //Plane p = new Plane(new Point3d(0, 0, 0), new Point3d(1, 0, 0), new Point3d(0, 1, 0));
+            
+            Queue<Curve> q = new Queue<Curve>();    // hold curves
+            q.Enqueue(crv);
+            Curve curCrv = null;
+
+            double minArea = double.PositiveInfinity;   // set max to min area
+            for (int i = 0; i < 1000; i++)
+            {
+                if (q.Count == 0) break;
+                curCrv = q.Dequeue();
+
+                AreaMassProperties currArea = AreaMassProperties.Compute(curCrv);   // count center point and area
+                if (currArea.Area < minArea) minArea = currArea.Area;   // if a curve is larger than previous one, break
+                else break;
+
+                Point3d centerPoint = currArea.Centroid;    // offset directions, could it be better?
+                if (output.Count !=0)   // check intersections between curves
+                {
+                    Rhino.Geometry.Intersect.CurveIntersections intersect =
+                    Rhino.Geometry.Intersect.Intersection.CurveCurve(curCrv, output[output.Count - 1], 0.001, 0.001);
+                    if (intersect.Count > 2) break; // if there are intersections, break
+                }
+                output.Add(curCrv); // add current curve to output
+                Curve[] nextCrvs = curCrv.Offset(centerPoint, new Vector3d(0, 0, 1), step, 0.001, CurveOffsetCornerStyle.Sharp);
+                if (nextCrvs == null) break;
+                foreach (Curve cv in nextCrvs)
+                {
+                    if (cv.IsClosed) q.Enqueue(cv);
+                }
+            }
+            DA.SetDataList(0, output);  // output list
         }
 
         public override GH_Exposure Exposure

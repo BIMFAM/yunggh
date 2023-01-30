5d69bc0f246988586d023418172e72bec6ed04aa
Matt Conway <matthewjosephconway@gmail.com>
2023-01-29 16:55:17 -0800

    Update UpdateCamera (working)

--- yunggh/Components/Document/UpdateCamera.cs
+++ yunggh/Components/Document/UpdateCamera.cs
@@ -17,8 +17,10 @@
 // IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
 // CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
-﻿using System;
+using System;
 using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
 
 using Grasshopper.Kernel;
 using Rhino.Geometry;
@@ -32,13 +34,6 @@ namespace yunggh
 {
     public class UpdateCamera : GH_Component
     {
-        /// <summary>
-        /// Each implementation of GH_Component must provide a public
-        /// constructor without any arguments.
-        /// Category represents the Tab in which the component will appear,
-        /// Subcategory the panel. If you use non-existing tab or panel names,
-        /// new tabs/panels will automatically be created.
-        /// </summary>
         public UpdateCamera()
           : base("Update Camera", "Update Camera",
               "Updates the viewport view.",
@@ -46,141 +41,58 @@ namespace yunggh
         {
         }
 
-        /// <summary>
-        /// Registers all the input parameters for this component.
-        /// </summary>
         protected override void RegisterInputParams(GH_Component.GH_InputParamManager pManager)
         {
-            // Use the pManager object to register your input parameters.
-            // You can often supply default values when creating parameters.
-            // All parameters must have the correct access type. If you want
-            // to import lists or trees of values, modify the ParamAccess flag.
-            pManager.AddPlaneParameter("Plane", "P", "Base plane for spiral", GH_ParamAccess.item, Plane.WorldXY);
-            pManager.AddNumberParameter("Inner Radius", "R0", "Inner radius for spiral", GH_ParamAccess.item, 1.0);
-            pManager.AddNumberParameter("Outer Radius", "R1", "Outer radius for spiral", GH_ParamAccess.item, 10.0);
-            pManager.AddIntegerParameter("Turns", "T", "Number of turns between radii", GH_ParamAccess.item, 10);
-
-            // If you want to change properties of certain parameters,
-            // you can use the pManager instance to access them by index:
-            //pManager[0].Optional = true;
+            pManager.AddTextParameter("Viewport", "V", "Viewport Name", GH_ParamAccess.item, "Perspetive");
+            pManager.AddPlaneParameter("Plane", "P", "Plane (Origin is Camera Location, YAxis is the Camera forward and ZAxis is Camera up).", GH_ParamAccess.item);
+            pManager[0].Optional = true;
         }
 
-        /// <summary>
-        /// Registers all the output parameters for this component.
-        /// </summary>
         protected override void RegisterOutputParams(GH_Component.GH_OutputParamManager pManager)
         {
-            // Use the pManager object to register your output parameters.
-            // Output parameters do not have default values, but they too must have the correct access type.
-            pManager.AddCurveParameter("Spiral", "S", "Spiral curve", GH_ParamAccess.item);
-
-            // Sometimes you want to hide a specific parameter from the Rhino preview.
-            // You can use the HideParameter() method as a quick way:
-            //pManager.HideParameter(0);
+            pManager.AddBooleanParameter("Updated", "U", "True when camera Updated", GH_ParamAccess.item);
         }
 
-        /// <summary>
-        /// This is the method that actually does the work.
-        /// </summary>
-        /// <param name="DA">The DA object can be used to retrieve data from input parameters and
-        /// to store data in output parameters.</param>
         protected override void SolveInstance(IGH_DataAccess DA)
         {
-            // First, we need to retrieve all data from the input parameters.
-            // We'll start by declaring variables and assigning them starting values.
+            //get inputs
+            string viewportName = "Perspective";
             Plane plane = Plane.WorldXY;
-            double radius0 = 0.0;
-            double radius1 = 0.0;
-            int turns = 0;
+            if (!DA.GetData(0, ref viewportName)) { }
+            if (!DA.GetData(1, ref plane)) { DA.SetData(0, false); return; }
 
-            // Then we need to access the input parameters individually.
-            // When data cannot be extracted from a parameter, we should abort this method.
-            if (!DA.GetData(0, ref plane)) return;
-            if (!DA.GetData(1, ref radius0)) return;
-            if (!DA.GetData(2, ref radius1)) return;
-            if (!DA.GetData(3, ref turns)) return;
+            //get Viewport to operate on
+            var views = Rhino.RhinoDoc.ActiveDoc.Views.ToList();
+            Rhino.Display.RhinoView view = null;
+            foreach (var v in views)
+            {
+                string name = v.MainViewport.Name;
+                if (name != viewportName) { continue; }
+                view = v;
+            }
+            if (view == null) { DA.SetData(0, false); return; }
 
-            // We should now validate the data and warn the user if invalid data is supplied.
-            if (radius0 < 0.0)
-            {
-                AddRuntimeMessage(GH_RuntimeMessageLevel.Error, "Inner radius must be bigger than or equal to zero");
-                return;
-            }
-            if (radius1 <= radius0)
-            {
-                AddRuntimeMessage(GH_RuntimeMessageLevel.Error, "Outer radius must be bigger than the inner radius");
-                return;
-            }
-            if (turns <= 0)
-            {
-                AddRuntimeMessage(GH_RuntimeMessageLevel.Error, "Spiral turn count must be bigger than or equal to one");
-                return;
-            }
+            //get viewport
+            var viewport = view.ActiveViewport;
+            if (viewport == null) { DA.SetData(0, false); return; }
 
-            // We're set to create the spiral now. To keep the size of the SolveInstance() method small,
-            // The actual functionality will be in a different method:
-            Curve spiral = CreateSpiral(plane, radius0, radius1, turns);
+            //update viewport
+            viewport.SetCameraLocation(plane.Origin, true);
+            viewport.SetCameraDirection(plane.YAxis, true);
+            viewport.CameraUp = plane.ZAxis;
+            view.Redraw();
 
             // Finally assign the spiral to the output parameter.
-            DA.SetData(0, spiral);
+            DA.SetData(0, true);
         }
 
-        private Curve CreateSpiral(Plane plane, double r0, double r1, Int32 turns)
-        {
-            Line l0 = new Line(plane.Origin + r0 * plane.XAxis, plane.Origin + r1 * plane.XAxis);
-            Line l1 = new Line(plane.Origin - r0 * plane.XAxis, plane.Origin - r1 * plane.XAxis);
-
-            Point3d[] p0;
-            Point3d[] p1;
-
-            l0.ToNurbsCurve().DivideByCount(turns, true, out p0);
-            l1.ToNurbsCurve().DivideByCount(turns, true, out p1);
-
-            PolyCurve spiral = new PolyCurve();
-
-            for (int i = 0; i < p0.Length - 1; i++)
-            {
-                Arc arc0 = new Arc(p0[i], plane.YAxis, p1[i + 1]);
-                Arc arc1 = new Arc(p1[i + 1], -plane.YAxis, p0[i + 1]);
-
-                spiral.Append(arc0);
-                spiral.Append(arc1);
-            }
-
-            return spiral;
-        }
-
-        /// <summary>
-        /// The Exposure property controls where in the panel a component icon
-        /// will appear. There are seven possible locations (primary to septenary),
-        /// each of which can be combined with the GH_Exposure.obscure flag, which
-        /// ensures the component will only be visible on panel dropdowns.
-        /// </summary>
         public override GH_Exposure Exposure
-        {
-            get { return GH_Exposure.primary; }
-        }
+        { get { return GH_Exposure.primary; } }
 
-        /// <summary>
-        /// Provides an Icon for every component that will be visible in the User Interface.
-        /// Icons need to be 24x24 pixels.
-        /// </summary>
         protected override System.Drawing.Bitmap Icon
-        {
-            get
-            {
-                return Resource.UpdateCamera;
-            }
-        }
+        { get { return Resource.UpdateCamera; } }
 
-        /// <summary>
-        /// Each component must have a unique Guid to identify it.
-        /// It is vital this Guid doesn't change otherwise old ghx files
-        /// that use the old ID will partially fail during loading.
-        /// </summary>
         public override Guid ComponentGuid
-        {
-            get { return new Guid("43d68cf2-f346-4467-a302-96162f0bd565"); }
-        }
+        { get { return new Guid("43d68cf2-f346-4467-a302-96162f0bd565"); } }
     }
 }

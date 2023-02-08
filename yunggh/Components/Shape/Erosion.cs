8e86b77b1d73a282384efb6f9c24c2ba9417a913
xuanhe95 <xuanhe95@gmail.com>
2023-02-08 01:51:45 -0800

    update for Erosion, comment for clearance

--- yunggh/Components/Shape/Erosion.cs
+++ yunggh/Components/Shape/Erosion.cs
@@ -32,26 +32,20 @@ namespace yunggh
             Double step = 0;
             if (!DA.GetData(0, ref crv) || !DA.GetData(1, ref step)) return;
             List<Curve> output = new List<Curve>();//output list
-                                                   //Plane p = new Plane(new Point3d(0, 0, 0), new Point3d(1, 0, 0), new Point3d(0, 1, 0));
 
-            Plane p = new Plane(new Point3d(0, 0, 0), new Point3d(1, 0, 0), new Point3d(0, 1, 0));
-
-            Queue <Curve> q = new Queue<Curve>();    // hold curves
+            Plane p = new Plane(new Point3d(0, 0, 0), new Point3d(1, 0, 0), new Point3d(0, 1, 0));  // using for offset
+            Queue<Curve> q = new Queue<Curve>();    // BFS hold curves
             q.Enqueue(crv);
-            Curve curCrv = null;
-
             double minArea = double.PositiveInfinity;   // set max to min area
-            for (int i = 0; i < 1000; i++)
+            for (int i = 0; i < 1000; i++)  //recursive limited times in case of infinite loop
             {
                 if (q.Count == 0) break;
-                curCrv = q.Dequeue();
-
+                Curve curCrv = q.Dequeue();
                 AreaMassProperties currArea = AreaMassProperties.Compute(curCrv);   // count center point and area
                 if (currArea.Area < minArea) minArea = currArea.Area;   // if a curve is larger than previous one, break
                 else break;
 
-                
-                if (output.Count !=0)   // check intersections between curves
+                if (output.Count != 0)   // check intersections between curves
                 {
                     Rhino.Geometry.Intersect.CurveIntersections intersect =
                     Rhino.Geometry.Intersect.Intersection.CurveCurve(curCrv, output[output.Count - 1], 0.001, 0.001);
@@ -59,15 +53,15 @@ namespace yunggh
                 }
                 output.Add(curCrv); // add current curve to output
 
-                // Point3d centerPoint = currArea.Centroid;    // offset directions, could it be better?
-                // Curve[] nextCrvs = curCrv.Offset(centerPoint, new Vector3d(0, 0, 1), step, 0.001, CurveOffsetCornerStyle.Sharp);
+                Point3d centerPoint = currArea.Centroid;    // offset directions, could it be better?
+                Curve[] nextCrvs = curCrv.Offset(centerPoint, new Vector3d(0, 0, 1), step, 0.001, CurveOffsetCornerStyle.Sharp);
 
-                Curve[] nextCrvs = curCrv.Offset(p, step, 0.001, CurveOffsetCornerStyle.Sharp); // offset by plane, this would be better!
+                //Curve[] nextCrvs = curCrv.Offset(p, step, 0.001, CurveOffsetCornerStyle.Sharp); // offset by plane, this would be better!
                 if (nextCrvs == null) break;
-                foreach (Curve cv in nextCrvs)
-                {
+                foreach (Curve cv in nextCrvs) {
                     if (cv.IsClosed) q.Enqueue(cv);
                 }
+                    
             }
             DA.SetDataList(0, output);  // output list
         }

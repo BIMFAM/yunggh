83ff643aa3b9d1eed585fb89da7aa336cd75e8f6
Matt Conway <matthewjosephconway@gmail.com>
2024-03-08 13:46:49 -0800

    WIP PyramidPanelization (Trimmed Surface Updates)

--- yunggh/Components/Panelization/PyramidPanelization.cs
+++ yunggh/Components/Panelization/PyramidPanelization.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 
 using Grasshopper.Kernel;
 using Rhino.Geometry;
@@ -87,10 +88,53 @@ namespace yunggh.Components.Panelization
         {
             var output = new List<Brep>();
 
-            foreach (var row in quadsByRow)
+            for (int r = 0; r < quadsByRow.Count; r++)
             {
-                foreach (var quad in row)
+                var row = quadsByRow[r];
+                for (int j = 0; j < row.Count; j++)
                 {
+                    var quad = row[j];
+                    if (quad[0] == Point3d.Unset || quad[1] == Point3d.Unset || quad[2] == Point3d.Unset || quad[3] == Point3d.Unset)
+                    {
+                        //find out how many points are unset
+                        int unsetPtCount = 0;
+                        foreach (var pt in quad)
+                        {
+                            if (pt != Point3d.Unset) { continue; }
+                            unsetPtCount++;
+                        }
+                        if (unsetPtCount > 1) { continue; } //continue if not enough points for triangle
+
+                        //create single quad triangle panel
+                        //Debug.WriteLine("D" + r + "-" + j);
+                        if (quad[0] == Point3d.Unset)
+                        {
+                            var pnl = NurbsSurface.CreateFromCorners(quad[1], quad[2], quad[3]);
+                            if (pnl == null) { continue; }
+                            output.Add(pnl.ToBrep());
+                        }
+                        else if (quad[1] == Point3d.Unset)
+                        {
+                            var pnl = NurbsSurface.CreateFromCorners(quad[0], quad[2], quad[3]);
+                            if (pnl == null) { continue; }
+                            output.Add(pnl.ToBrep());
+                        }
+                        else if (quad[2] == Point3d.Unset)
+                        {
+                            var pnl = NurbsSurface.CreateFromCorners(quad[0], quad[1], quad[3]);
+                            if (pnl == null) { continue; }
+                            output.Add(pnl.ToBrep());
+                        }
+                        else if (quad[3] == Point3d.Unset)
+                        {
+                            var pnl = NurbsSurface.CreateFromCorners(quad[0], quad[1], quad[2]);
+                            if (pnl == null) { continue; }
+                            output.Add(pnl.ToBrep());
+                        }
+                        //Debug.WriteLine("C" + r + "-" + j);
+                        continue; //continue because quad is triangle
+                    }
+
                     //get pyramid tip
                     var center = (quad[0] + quad[1] + quad[2] + quad[3]) / 4.00;
                     var srf = brep.Surfaces[0];
@@ -112,8 +156,12 @@ namespace yunggh.Components.Panelization
                         {
                             pt1 = quad[i];
                         }
+                        //Debug.WriteLine("A" + r + "-" + j + "-" + i);
                         var pnl = NurbsSurface.CreateFromCorners(pt0, pt1, center);
+                        if (pnl == null) { continue; }
+                        //Debug.WriteLine("B");
                         output.Add(pnl.ToBrep());
+                        //Debug.WriteLine("C");
                     }
                 }
             }
@@ -123,23 +171,116 @@ namespace yunggh.Components.Panelization
 
         public static List<List<List<Point3d>>> GetQuadCorners(List<Curve> uCrvs, List<Curve> vCrvs)
         {
-            //getting all curve intersection points
-            double tol = Rhino.RhinoDoc.ActiveDoc.ModelAbsoluteTolerance;
+            //setting up a complete grid with empty point values
             var pts = new List<List<Point3d>>();
             for (int u = 0; u < uCrvs.Count; u++)
             {
-                var uCrv = uCrvs[u];
                 var crvPts = new List<Point3d>();
+                for (int v = 0; v < vCrvs.Count; v++)
+                {
+                    crvPts.Add(Point3d.Unset);
+                }
+                pts.Add(crvPts);
+            }
+
+            //getting all curve intersection points
+            double tol = Rhino.RhinoDoc.ActiveDoc.ModelAbsoluteTolerance;
+            for (int u = 0; u < pts.Count; u++)
+            {
+                var uCrv = uCrvs[u];
                 for (int v = 0; v < vCrvs.Count; v++)
                 {
                     var vCrv = vCrvs[v];
                     var crvXs = Rhino.Geometry.Intersect.Intersection.CurveCurve(uCrv, vCrv, tol, tol);
-                    foreach (var crvX in crvXs)
+                    if (crvXs.Count == 0) { continue; }
+                    foreach (var crvX in crvXs) { pts[u][v] = crvX.PointA; }
+                }
+            }
+
+            //find end points that might not have been an intersection
+            for (int u = 1; u < pts.Count - 1; u++)
+            {
+                //Debug.WriteLine("A");
+                var uCrv = uCrvs[u - 1];
+                //Debug.WriteLine("B");
+
+                
+                for (int v = 1; v < vCrvs.Count - 1; v++)
+                {
+                    int uPointsFound = 0;
+                    int vPointsFound = 0;
+                    //Debug.WriteLine("C" + u + "-" + v);
+                    var vCrv = vCrvs[v - 1];
+                    //Debug.WriteLine("D" + u + "-" + v);
+                    var pt = pts[u][v];
+                    //Debug.WriteLine("E" + u + "-" + v);
+                    if (pt == Point3d.Unset) { continue; }
+                  
+                    var ptUS = pts[u - 1][v];
+                    if (ptUS == Point3d.Unset && uPointsFound < 2)
                     {
-                        crvPts.Add(crvX.PointA);
+                        //Debug.WriteLine("F" + u + "-" + v);
+                        var start = uCrv.PointAtStart;
+                        var end = uCrv.PointAtEnd;
+                        if(start.DistanceTo(pt) > tol && end.DistanceTo(pt) > tol)
+                        {
+                            if (pt.DistanceTo(start) < pt.DistanceTo(end))
+                                pts[u - 1][v] = start;
+                            else
+                                pts[u - 1][v] = end;
+                            uPointsFound++;
+                        }                                     
+                        //Debug.WriteLine("G" + u + "-" + v);
+                    }
+                    var ptVS = pts[u][v - 1];
+                    if (ptVS == Point3d.Unset && vPointsFound < 2)
+                    {
+                        //Debug.WriteLine("H" + u + "-" + v);
+                        var start = vCrv.PointAtStart;
+                        var end = vCrv.PointAtEnd;
+                        if (start.DistanceTo(pt) > tol && end.DistanceTo(pt) > tol)
+                        {
+                            if (pt.DistanceTo(start) < pt.DistanceTo(end))
+                                pts[u][v - 1] = start;
+                            else
+                                pts[u][v - 1] = end;
+                            vPointsFound++;
+                        }
+                        //Debug.WriteLine("I" + u + "-" + v);
+                    }
+                    var ptUE = pts[u + 1][v];
+                    if (ptUE == Point3d.Unset && uPointsFound < 2)
+                    {
+                        //Debug.WriteLine("J" + u + "-" + v);
+                        var start = uCrv.PointAtStart;
+                        var end = uCrv.PointAtEnd;
+                        if (start.DistanceTo(pt) > tol && end.DistanceTo(pt) > tol)
+                        {
+                            if (pt.DistanceTo(start) < pt.DistanceTo(end))
+                                pts[u + 1][v] = start;
+                            else
+                                pts[u + 1][v] = end;
+                            uPointsFound++;
+                        }
+                        //Debug.WriteLine("K" + u + "-" + v);
+                    }
+                    var ptVE = pts[u][v + 1];
+                    if (ptVE == Point3d.Unset && vPointsFound < 2)
+                    {
+                        //Debug.WriteLine("L" + u + "-" + v);
+                        var start = vCrv.PointAtStart;
+                        var end = vCrv.PointAtEnd;
+                        if (start.DistanceTo(pt) > tol && end.DistanceTo(pt) > tol)
+                        {
+                            if (pt.DistanceTo(start) < pt.DistanceTo(end))
+                                pts[u][v + 1] = start;
+                            else
+                                pts[u][v + 1] = end;
+                            vPointsFound++;
+                        }
+                        //Debug.WriteLine("M" + u + "-" + v);
                     }
                 }
-                pts.Add(crvPts);
             }
 
             //grouping points into quads
